c7c1cd1d7e0ac4081d6ee8309eb08e2843d46ea6
siabn4 <sindhuja27@vt.edu>
2026-07-21 12:57:52 -0400

    Initial Spring Boot project setup

---
+++ src/main/java/com/sindhu/promotionmanager/PromotionmanagerApplication.java
@@ -0,0 +1,13 @@
+package com.sindhu.promotionmanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PromotionmanagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PromotionmanagerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sindhu/promotionmanager/PromotionmanagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sindhu.promotionmanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PromotionmanagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
